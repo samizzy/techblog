360cf79d68ae2403d24c85c978b07a3f1554f54c
Samrat Saha <samizzy18@gmail.com>
2026-06-28 23:43:01 +0200

    move to vuepress home theme

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,120 +1,42 @@
 import { defineUserConfig } from "vuepress";
-import recoTheme from "vuepress-theme-reco";
-import { viteBundler } from '@vuepress/bundler-vite'
-import { webpackBundler } from '@vuepress/bundler-webpack'
+import { hopeTheme } from "vuepress-theme-hope";
+import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
   title: "BakaCoder",
-  // description: "Just playing around",
   bundler: viteBundler(),
-  // bundler: webpackBundler(),
-  head: [
-    ['link', {rel: 'icon', href: '/suspicious_eyes.png'}]
-  ],
-  theme: recoTheme({
-    socialLinks: [
-    { icon: 'IconGithub', link: 'https://github.com/samizzy'}
-  ],
+  head: [["link", { rel: "icon", href: "/suspicious_eyes.png" }]],
+  theme: hopeTheme({
+    hostname: "https://bakacoder.com",
     logo: "/gintoki_logo.png",
-    author: "samizzy",
-    authorAvatar: "/thorfinn_avatar.jpg",
+    favicon: "/suspicious_eyes.png",
+    author: {
+      name: "Samrat Saha",
+      url: "https://github.com/samizzy",
+    },
     docsRepo: "https://github.com/samizzy/techblog",
     docsBranch: "master",
-    docsDir: "docs",
-    lastUpdatedText: "Last Updated",
-    // series 为原 sidebar
-    series: {
-      "/docs/theme-reco/": [
-        {
-          text: "module one",
-          children: ["home", "theme"],
-        },
-        {
-          text: "module two",
-          children: ["api", "plugin"],
-        },
-      ],
-    },
+    docsDir: ".",
+    lastUpdated: true,
     navbar: [
       { text: "Home", link: "/" },
-      { text: "Categories", link: "/categories/technology/1.html" },
-      { text: "Tags", link: "/tags/java/1.html" },
-      // { text: "Tags", link: "/tags/java/1.html" },
-      // {
-      //   text: "Docs",
-      //   children: [
-      //     { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
-      //     { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
-      //   ],
-      // },
+      { text: "Categories", link: "/category/" },
+      { text: "Tags", link: "/tag/" },
     ],
-    // bulletin: {
-    //   body: [
-    //     {
-    //       type: "text",
-    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "QQ 群",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li>QQ群1：1037296104</li>
-    //         <li>QQ群2：1061561395</li>
-    //         <li>QQ群3：962687802</li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "GitHub",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "buttongroup",
-    //       children: [
-    //         {
-    //           text: "打赏",
-    //           link: "/docs/others/donate.html",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
-    // // commentConfig: {
-    //   type: 'valine',
-    //   // options 与 1.x 的 valineConfig 配置一致
-    //   options: {
-    //     // appId: 'xxx',
-    //     // appKey: 'xxx',
-    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
-    //     // verify: true, // 验证码服务
-    //     // notify: true,
-    //     // recordIP: true,
-    //     // hideComments: true // 隐藏评论
-    //   },
-    // },
+    sidebar: false,
+    blog: {
+      avatar: "/thorfinn_avatar.jpg",
+      roundAvatar: true,
+      name: "samizzy",
+      description:
+        "Notes from a backend engineer navigating clean code, broken builds, and the occasional existential crisis",
+      medias: {
+        GitHub: "https://github.com/samizzy",
+      },
+    },
+    darkmode: "toggle",
+    plugins: {
+      blog: true,
+    },
   }),
-  // debug: true,
 });
